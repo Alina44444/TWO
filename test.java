d859593f3071604539c7b989f502837150158b63
Alina44444 <alina.valiyeva.s@mail.ru>
2024-08-01 21:34:19 +0500

    где класс

--- test.java
+++ test.java
@@ -2,5 +2,6 @@ public class test {
     public test() {
         System.out.println("создание репозитория TWO");
         System.out.println("новый коммит второй ");
+        System.out.println("где класс ");
     }
 }
