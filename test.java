9d3b13336a7604435642fb2f4f8c8bb75293380d
Alina44444 <alina.valiyeva.s@mail.ru>
2024-08-01 22:04:18 +0500

    git чекаут бля

--- test.java
+++ test.java
@@ -3,6 +3,7 @@ public class test {
         System.out.println("создание репозитория TWO");
         System.out.println("новый коммит второй ");
         System.out.println("где класс ");
+        System.out.println("git checkout main выход из просмотра коммита ");
         /*
         git
         git branch
