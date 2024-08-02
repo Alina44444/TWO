6b5c998ffc81af6e2bdf964fdc8421e1c5c03760
Alina44444 <alina.valiyeva.s@mail.ru>
2024-08-02 19:48:40 +0500

    not 1

--- newOne.java
+++ newOne.java
@@ -2,5 +2,6 @@ public class newOne {
     public newOne() {
         System.out.println("новая ветка ");
         System.out.println("не удалось слияние ");
+        System.out.println("не удалось слияние two");
     }
 }
